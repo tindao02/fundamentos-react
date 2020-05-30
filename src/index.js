e5fb2e742611bd45ff0a1ba343f076d3e21f3f11
Manoel Tindão <timdao02@hotmail.com>
2020-05-30 02:05:04 -0300

    Primeiro Componente

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import React from 'react'
 
 import './index.css'
 
+import Primeiro from './components/basicos/Primeiro'
+
 const tag = 'Olá mundo!'
 
 ReactDom.render
 (
-    <i>Olá React
-       {tag}
+    <i> 
+        <Primeiro></Primeiro>
     </i>, 
     document.getElementById('root')
 )

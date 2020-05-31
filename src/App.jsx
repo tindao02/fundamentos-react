19b925a7b17e1938e415d697671a1badd6e9db0f
Manoel Tindão <timdao02@hotmail.com>
2020-05-30 23:20:42 -0300

    Componente Card layout Flex

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
 import React from 'react';
 
+import './App.css'
+
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
 import Randomico from './components/basicos/Randomico'
 import Card from './components/layout/Card'
 
+
 export default () =>
 (
-    <div id="app">
+    <div className="App">
         <h1>Fundamento React</h1>
-        <h3>Todo esquerdida é veado</h3>
 
-        <Card titulo="#04 - Desafio Aleatório">
-            Randomico: <Randomico min={5} max={10}/>
-        </Card>
+        <div className='Cards'>
+            <Card titulo="#04 - Desafio Aleatório">
+                Randomico: <Randomico min={5} max={10}/>
+            </Card>
 
-        <Card titulo="#03 - Fragmento">
-            <Fragmento />
-        </Card>
-        
-        <Card titulo="#02 - Componente Com parâmetros">
-            <ComParametro
-                titulo="Situação do aluno"
-                aluno="Pedro"
-                nota={9.3}
-            />
-        </Card>
-        
-        <Card titulo="#01 - Primeiro Componente">
-            <Primeiro/>
-        </Card>
+            <Card titulo="#03 - Fragmento">
+                <Fragmento />
+            </Card>
+            
+            <Card titulo="#02 - Componente Com parâmetros">
+                <ComParametro
+                    titulo="Situação do aluno"
+                    aluno="Pedro"
+                    nota={9.3}
+                />
+            </Card>
+            
+            <Card titulo="#01 - Primeiro Componente">
+                <Primeiro/>
+            </Card>
+        </div>
     </div>
 );

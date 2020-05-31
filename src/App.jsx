aeadaa88f1a1eb2510a3d4e3029da32332be4870
Manoel Tindão <timdao02@hotmail.com>
2020-05-30 23:04:53 -0300

    Componente Card e divisão

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,32 @@ import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
 import Randomico from './components/basicos/Randomico'
+import Card from './components/layout/Card'
 
 export default () =>
 (
     <div id="app">
         <h1>Fundamento React</h1>
-        Randomico: <Randomico min={5} max={10}/>
-        <Fragmento />
-        <ComParametro
-            titulo="Situação do aluno"
-            aluno="Pedro"
-            nota={9.3}
-        />
-        <Primeiro></Primeiro>
+        <h3>Todo esquerdida é veado</h3>
+
+        <Card titulo="#04 - Desafio Aleatório">
+            Randomico: <Randomico min={5} max={10}/>
+        </Card>
+
+        <Card titulo="#03 - Fragmento">
+            <Fragmento />
+        </Card>
+        
+        <Card titulo="#02 - Componente Com parâmetros">
+            <ComParametro
+                titulo="Situação do aluno"
+                aluno="Pedro"
+                nota={9.3}
+            />
+        </Card>
+        
+        <Card titulo="#01 - Primeiro Componente">
+            <Primeiro/>
+        </Card>
     </div>
 );
--- src/components/basicos/Randomico.jsx
+++ src/components/basicos/Randomico.jsx
@@ -4,8 +4,8 @@ export default (props) =>
 {
     const { min, max } = props;
     return(
-        <>
+        <p>
             {parseInt(Math.random() * (max+1 - min)) + min}
-        </>
+        </p>
     )
 }
